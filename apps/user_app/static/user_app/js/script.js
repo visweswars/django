5ff54eebf38c09c52d144915b58845c50b264553
Mauricio Ruanova-Hurtado <mruanova@yahoo.com>
2017-05-31 20:41:06 -0500

    new python django project

--- apps/user_app/static/user_app/js/script.js
+++ apps/user_app/static/user_app/js/script.js
@@ -12,8 +12,7 @@ function initialize() {
     };
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
 };
-/*
-function addMarker(index,title,website,address) {
+function addMarker(index,title,address) {
     geocoder.geocode({ 'address': address }, function (results, status) {
         if (status == 'OK') {
             var marker = new google.maps.Marker({
@@ -23,9 +22,7 @@ function addMarker(index,title,website,address) {
             });
             markers.push(marker);
             var contentString = '<div id="content">' +
-                '<div class="link2">' + title + '</div>' +
                 '<div id="bodyContent">' +
-                '<div class="website">' + website + '</div>' +
                 '<div class="address">'+ address + '</div>' +
                 '</div>' +
                 '</div>';
@@ -42,29 +39,10 @@ function addMarker(index,title,website,address) {
     });
 };
 $(document).ready(function () {
-    var api = "https://246gg84zg8.execute-api.us-west-2.amazonaws.com/prod/projects";
-    console.log(api);
-    $.get(api, function (response) {
-        response.Items.sort(function (a, b) {
-            return parseFloat(a.ProjectId) - parseFloat(b.ProjectId);
-        });
-        for (var i = 0; i < response.Items.length; i++) {
-            if(i<5){
-                var project = "<div id='project"+response.Items[i].ProjectId+"' class='project'>"
-                    + "<div class='company'>"+response.Items[i].Name+"</div>"
-                    + "<div class='website'>"+response.Items[i].Website+"</div>"
-                    + "<div class='position'>"+response.Items[i].Position+"</div>"
-                    + "<div class='address'>"+response.Items[i].Address+"</div>"
-                    + "</div>";
-                console.log(response.Items[i].ProjectId);
-                console.log(response.Items[i].Name);
-                console.log(response.Items[i].Website);
-                console.log(response.Items[i].Position);
-                console.log(response.Items[i].Address);
-                $("#projects").append(project);
-                addMarker(i,response.Items[i].Name,response.Items[i].Website,response.Items[i].Address);
-            }
-        };
-    }, "json");
+    console.log("ready");
+    $("li").each(function( index ) {
+        console.log( $( this ).text() );                
+        addMarker(index,"Zillow",$( this ).text());
+
+    });
 });
-*/
